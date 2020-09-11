363c61f47d88c9c47d972553f4df9bdbb18a1ea8
Alex Ozdemir <aozdemir@hmc.edu>
2020-09-11 13:58:27 -0700

    Clean up rug-fft benchmarks

--- rug-fft/benches/fft.rs
+++ rug-fft/benches/fft.rs
@@ -7,8 +7,8 @@ use rug::Integer;
 use std::str::FromStr;
 use test::Bencher;
 
-#[bench]
-fn bit_rev(b: &mut Bencher) {
+fn bench_fft<F: Fn(&mut [Integer], &Integer, &Integer)>(fft: F, b: &mut Bencher) {
+    // BLS12-381 Scalar field.
     let p = Integer::from_str(
         "52435875175126190479447740508185965837690552500527637822603658699938581184513",
     )
@@ -18,7 +18,7 @@ fn bit_rev(b: &mut Bencher) {
     )
     .unwrap();
     let s = 32;
-    let n = 10;
+    let n = 8;
     assert!(n <= s);
     let w = w_base.pow_mod(&Integer::from(s - n), &p).unwrap();
     let mut rng = rug::rand::RandState::new();
@@ -26,52 +26,21 @@ fn bit_rev(b: &mut Bencher) {
         .take(1 << n)
         .collect::<Vec<_>>();
     b.iter(|| {
-        bit_rev_radix_2_ntt(&mut ys, &p, &w);
+        fft(&mut ys, &p, &w);
     });
+}
+
+#[bench]
+fn bit_rev(b: &mut Bencher) {
+    bench_fft(rug_fft::bit_rev_radix_2_ntt, b);
 }
 
 #[bench]
 fn cooley_tukey(b: &mut Bencher) {
-    let p = Integer::from_str(
-        "52435875175126190479447740508185965837690552500527637822603658699938581184513",
-    )
-    .unwrap();
-    let w_base = Integer::from_str(
-        "10238227357739495823651030575849232062558860180284477541189508159991286009131",
-    )
-    .unwrap();
-    let s = 32;
-    let n = 10;
-    assert!(n <= s);
-    let w = w_base.pow_mod(&Integer::from(s - n), &p).unwrap();
-    let mut rng = rug::rand::RandState::new();
-    let mut ys = std::iter::repeat_with(|| Integer::from(p.random_below_ref(&mut rng)))
-        .take(1 << n)
-        .collect::<Vec<_>>();
-    b.iter(|| {
-        cooley_tukey_radix_2_ntt(&mut ys, &p, &w);
-    });
+    bench_fft(rug_fft::cooley_tukey_radix_2_ntt, b);
 }
 
 #[bench]
 fn naive(b: &mut Bencher) {
-    let p = Integer::from_str(
-        "52435875175126190479447740508185965837690552500527637822603658699938581184513",
-    )
-    .unwrap();
-    let w_base = Integer::from_str(
-        "10238227357739495823651030575849232062558860180284477541189508159991286009131",
-    )
-    .unwrap();
-    let s = 32;
-    let n = 5;
-    assert!(n <= s);
-    let w = w_base.pow_mod(&Integer::from(s - n), &p).unwrap();
-    let mut rng = rug::rand::RandState::new();
-    let mut ys = std::iter::repeat_with(|| Integer::from(p.random_below_ref(&mut rng)))
-        .take(1 << n)
-        .collect::<Vec<_>>();
-    b.iter(|| {
-        naive_ntt(&ys, &p, &w);
-    });
+    bench_fft(rug_fft::naive_ntt, b);
 }
