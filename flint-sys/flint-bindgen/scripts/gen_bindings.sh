567b37a8da1000115dc2f3ff6f199bef9427eac7
aedoq <aedoq@protonmail.com>
2026-02-26 13:34:12 +0100

    Automate flint-sys binding generation
    From the flint-sys directory, run
    flint-bindgen/scripts/gen_bindings.sh to generate bindings for a new
    FLINT version (takes a few minutes).
    
    The header list is now automatically extracted from a FLINT build and
    split into two parts: Headers copied from the build and headers we
    generate bindings for as part of the public API.
    
    The header list and list of module declarations are split out into
    their own files and included via `include!` so they do not have to be
    inserted manually. Note that `__mod_statements.rs` has to be in
    flint-sys/src because rustc searches for module files relative to the
    file containing the `mod` statement, and *not* the file it is
    `include!`d from.
    
    The copy of extern.c flint-sys/flint-bindgen/scripts has been removed,
    and extern.c is now directly placed into flint-sys/C. Manual editing
    of this file should not be required.
    
    Manual editing of generated files in general should only be necessary
    to fix errors in the bindings by removing `use` statements from
    binding modules. This is because bindgen will generate type names that
    are not globally unique like `__bindgen_ty_1` and pulling those in
    from two different modules can cause problems when referenced.
    
    The bindings present in this commit are *not* yet generated via this mechanism.

---
+++ flint-sys/flint-bindgen/scripts/gen_bindings.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+set -euo pipefail
+# Invoke from flint-sys as flint-bindgen/scripts/gen_bindings.sh
+
+logdir=$(realpath flint-bindgen/scripts/logs)
+mkdir -p ${logdir}
+# rm ${logdir}/*
+
+echo "Removing old build artifacts..."
+cargo clean
+rm -f C/extern.c src/__mod_statements.rs
+pushd flint-bindgen
+cargo clean
+rm -f scripts/bindgen_headers.rs scripts/build_headers.rs
+popd
+echo "Starting flint-sys build to get gmp and mpfr..."
+cargo b >${logdir}/00-gmp_mpfr.log 2>&1 || true
+gmp_mpfr_dir=$(realpath $(find target/debug/build -type d -name "gmp-mpfr-sys-*" -execdir test -d "{}/out" \; -print))
+echo "Building FLINT..."
+rm -rf flint-bindgen/flint-build
+rm -rf flint-bindgen/flint-out
+mkdir -p flint-bindgen/flint-build
+mkdir -p flint-bindgen/flint-out
+pushd flint-bindgen/flint-build
+
+echo "  Running configure..."
+../../flint-3.1.2/configure --disable-shared \
+    --with-mpfr=${gmp_mpfr_dir}/out \
+    --with-gmp=${gmp_mpfr_dir}/out \
+    CFLAGS="-fPIC" \
+    --prefix=$(realpath ../flint-out) > ${logdir}/01-flint-configure.log
+echo "  Running make..."
+make -j $(nproc) > ${logdir}/02-flint-make.log
+echo "  Running make install to get public header files..."
+make install > ${logdir}/03-flint-install.log
+
+popd
+
+echo "  FLINT build success"
+echo "Generating header list..."
+pushd flint-bindgen/scripts
+python headers.py ../flint-out/include/flint
+popd
+echo "Generating bindings..."
+pushd flint-bindgen
+INCLUDE_DIR=flint-out/include cargo b -vv >${logdir}/04-bindgen.log 2>&1
+bindings_dir=$(realpath $(find target/debug/build -type d -name "flint-bindgen-*" -execdir test -d "{}/out" \; -print))
+popd
+echo "Consolidating function wrappers..."
+python flint-bindgen/scripts/externs.py ${bindings_dir}/out/extern > C/extern.c
+echo "Removing stale binding modules..."
+find src -type f -name "*.rs" -not \( -name "lib.rs" -o -name "bindgen.rs" -o -name "deps.rs" \) -print0 | xargs -0 rm
+echo "Installing new modules..."
+mv flint-bindgen/scripts/__mod_statements.rs src/
+cp ${bindings_dir}/out/*.rs src/
+echo "Fixing warnings (unused imports)..."
+cargo fix --lib -p flint-sys --allow-dirty --broken-code >${logdir}/05-warnings.log 2>&1
